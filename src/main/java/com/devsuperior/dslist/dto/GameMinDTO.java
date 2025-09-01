383747961d49dbeab10959903434130aeb692acf
Dimas Rocha <dimas.dasp@gmail.com>
2025-08-31 21:06:10 -0300

    GET Gamelist ID

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.Entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -16,7 +17,15 @@ public class GameMinDTO {
 	public GameMinDTO(Game entity) {
 		
 		id = entity.getId();
-		title = entity.getGenre();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+public GameMinDTO(GameMinProjection entity) {
+		
+		id = entity.getId();
+		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
